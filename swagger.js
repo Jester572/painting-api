88fef876c330c67ce1de4a2868594da7fc03a078
Jester572 <jester572@yahoo.com>
2023-07-11 01:52:48 -0600

    Last attempt (hopfully)

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'My API',
         description: 'Description'
     },
-    host: 'cse341-paintings.onrender.com/',
+    host: 'cse341-paintings.onrender.com',
     basePath: '/',
     schemes: ['https']
 };
