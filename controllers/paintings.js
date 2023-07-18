ffdf1f81a1111ad013c54470e1f7b8b9ef370d2b
Jester572 <jester572@yahoo.com>
2023-07-18 01:11:33 -0600

    Added try catch blocks for all routes in paintings

--- controllers/paintings.js
+++ controllers/paintings.js
@@ -7,21 +7,29 @@ const database = mongodb.getDb().db('paintings').collection('paintings');
 //Paintings
 
 const getPaintings = async (req, res) => {
-    const results = await database.find();
-    results.toArray().then((lists) => {
-        res.setHeader('content-Type', 'application/json');
-        res.status(200).json(lists);
-    });
+    try {
+        const results = await database.find();
+        results.toArray().then((lists) => {
+            res.setHeader('content-Type', 'application/json');
+            res.status(200).json(lists);
+        });
+    } catch (error){
+        res.status(500).json(res.error || 'Error occurred while retrieving paintings');
+    }
 };
 
 const getSinglePainting = async (req, res) => {
-    const paintingId = new ObjectId(req.params.id);
-    console.log(paintingId);
-    const results = await database.find({_id: paintingId});
-    results.toArray().then((lists) => {
-        res.setHeader('content-Type', 'application/json');
-        res.status(200).json(lists[0]);
-    });
+    try {
+        const paintingId = new ObjectId(req.params.id);
+
+        const results = await database.find({_id: paintingId});
+        results.toArray().then((lists) => {
+            res.setHeader('content-Type', 'application/json');
+            res.status(200).json(lists[0]);
+        });
+    } catch (error) {
+
+    }
 };
 
 const addPainting = async (req, res) => {
@@ -36,9 +44,9 @@ const addPainting = async (req, res) => {
             url: req.body.url
         };
 
-        const result = await PaintingSchema.validateAsync(newPainting)
-
+        const result = await PaintingSchema.validateAsync(newPainting);
         const response = await database.insertOne(newPainting);
+
         if (response.acknowledged) {
             res.status(201).json(response);
         } else {
@@ -52,52 +60,73 @@ const addPainting = async (req, res) => {
 };
 
 const editPainting = async (req, res) => {
-    const paintingId = new ObjectId(req.params.id);
-    const newPainting = {
-        painting_title: req.body.painting_title,
-        artist: req.body.artist,
-        date_created: req.body.date_created,
-        period: req.body.period,
-        keywords: req.body.keywords,
-        location: req.body.location,
-        url: req.body.url
-    };
-    const response = await database.replaceOne({_id: paintingId}, newContact);
-    if (response.acknowledged) {
-        res.status(20).json(response);
-    } else {
-        res.status(500).json(response.error || 'Error occurred while updating painting');
+
+    try{
+        const paintingId = new ObjectId(req.params.id);
+        const newPainting = {
+            painting_title: req.body.painting_title,
+            artist: req.body.artist,
+            date_created: req.body.date_created,
+            period: req.body.period,
+            keywords: req.body.keywords,
+            location: req.body.location,
+            url: req.body.url
+        };
+
+        const result = await PaintingSchema.validateAsync(newPainting);
+        const response = await database.replaceOne({_id: paintingId}, newContact);
+
+        if (response.acknowledged) {
+            res.status(201).json(res);
+        } else {
+            res.status(500).json(res.error || 'Error occurred while adding painting');
+        }
+    } catch (error){
+        if (error.isJoi === true) {
+            res.status(422).json(error);
+        }
     }
 
 };
 
 const deletePainting = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
-    const response = await database.deleteOne({_id: userId});
 
-    if (response.acknowledged) {
-        res.status(204).json(response);
-    } else {
-        res.status(500).json(response.error || 'Error occurred while deleting Painting');
+    try {
+        const userId = new ObjectId(req.params.id);
+        const response = await database.deleteOne({_id: userId});
+
+        if (response.acknowledged) {
+            res.status(204).json(res);
+        } 
+    } catch(error) {
+        res.status(500).json(res.error || 'Error occurred while deleting Painting');
     }
 };
 
 //Artists
 
 const getArtists = async (req, res) => {
-    const results = await database.distinct("artist");
-    // console.log(await results.toArray());
-    // results.toArray().then((lists) => {
-    //     artists = lists.map((list) => {return list.artist})
-    res.setHeader('content-Type', 'application/json');
-    res.status(200).json(results);
+    try {
+        const results = await database.distinct("artist");
+        // console.log(await results.toArray());
+        // results.toArray().then((lists) => {
+        //     artists = lists.map((list) => {return list.artist})
+        res.setHeader('content-Type', 'application/json');
+        res.status(200).json(res);
+    } catch (error) {
+        res.status(500).json(res.error || 'Error occurred while retrieving artists');
+    }
     // });
 };
 
 const getPaintingNames = async (req, res) => {
-    const results = await database.distinct("painting_title")
-    res.setHeader('content-Type', 'application/json');
-    res.status(200).json(results);
+    try {
+        const results = await database.distinct("painting_title")
+        res.setHeader('content-Type', 'application/json');
+        res.status(200).json(results);
+    } catch(error) {
+        res.status(500).json(res.error || 'Error occurred while retrieving paintings');
+    }
 };
 
 
