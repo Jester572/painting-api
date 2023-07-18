fb541c66e4827520cc210d9696ed8503988af195
Jester572 <jester572@yahoo.com>
2023-07-18 00:51:24 -0600

    Updated swagger to use the joi schema

--- swagger.js
+++ swagger.js
@@ -1,14 +1,22 @@
 const swaggerAutogen = require('swagger-autogen')();
+const j2s = require('joi-to-swagger');
+const schema = require('./schemas/index');
+
+const { swagger: painting } = j2s(schema.PaintingSchema)
 
 const doc = {
     info: {
         title: 'My API',
         description: 'Description'
     },
+    '@definitions': {
+        swagger: painting,
+    },
     host: 'cse341-paintings.onrender.com',
     basePath: '/',
     schemes: ['https']
 };
+console.log(doc);
 
 const outputFile = './swagger-output.json';
 const endpointsFiles = ['./server.js'];
